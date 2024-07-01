580c5a192f8c25dfc999ac1277e98b762cf3d3ce
João Oliveira <jaoovit@gmail.com>
2024-07-01 16:24:13 +0100

    create a Navbar with a search form and a link to comeback to the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+{/*JSX documents*/}
+import Home from './pages/Home'
+import NavBar from './components/Navbar'
+
 import './App.css'
 
 function App() {
@@ -5,7 +9,8 @@ function App() {
 
   return (
     <>
-      
+      <NavBar />
+      <Home />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
+import { Link } from "react-router-dom"
+import { MdMovieFilter } from "react-icons/md";
+import { FaSearch } from "react-icons/fa";
+
 const Navbar = () => {
   return (
     <div>
-      
+      <Link to="/"><MdMovieFilter />Movie Library</Link>
+      <form>
+        <input type="text" placeholder="Search a movie"/>
+        <button type="submit"><FaSearch /></button>
+      </form>
     </div>
   )
 }
