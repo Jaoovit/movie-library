f6b6519ee1e77605062b63e14a336703241acd54
João Oliveira <jaoovit@gmail.com>
2024-07-02 16:35:31 +0100

    config the navbar to search movies and display in the search page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,25 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { MdMovieFilter } from "react-icons/md";
 import { FaSearch } from "react-icons/fa";
+import { useState } from "react";
 
 const Navbar = () => {
+
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!search) return
+    navigate(`/search?q=${search}`)
+    setSearch("")
+  }
+
   return (
     <div>
       <Link to="/"><MdMovieFilter />Movie Library</Link>
-      <form>
-        <input type="text" placeholder="Search a movie"/>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Search a movie" value={search} onChange={(e) => {setSearch(e.target.value)}}/>
         <button type="submit"><FaSearch /></button>
       </form>
     </div>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,37 @@
+import { useState, useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
+
+const apiKey = import.meta.env.VITE_API_KEY;
+  const search = import.meta.env.VITE_API_SEARCH;
+
 const Search = () => {
+
+  const [searchParams] = useSearchParams()
+
+  const [searchMovies, setSearchMovies] = useState([]);
+  const query = searchParams.get("q");
+
+  const getSearchMovie = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setSearchMovies(data.results)
+  }
+
+  useEffect(() => {
+    const searchMoviesUrl = `${search}?${apiKey}&query=${query}`
+    getSearchMovie(searchMoviesUrl)
+  }, [query])
+
   return (
     <div>
-      
+      <div>
+        <h2>Results for: <span>{query}</span></h2>
+        <div>
+          {searchMovies.length > 0 && searchMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+        </div>
+      </div>
     </div>
   )
 }
