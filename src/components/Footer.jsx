dab61260654ee4cb1283b8357e540163e3c3841e
João Oliveira <jaoovit@gmail.com>
2024-07-02 18:33:05 +0100

    add style to footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,13 @@
 import { FaGithub } from "react-icons/fa";
 
+import '../index.css'
+
 const Footer = () => {
   return (
     <div>
-      <footer>Copyright &copy;
-            <a href="https://github.com/Jaoovit">João Vitor de Oliveira
-            <FaGithub /></a>
+      <footer className="flex justify-center p-9 text-2xl">Copyright &copy;
+            <a className="flex items-center gap-3" href="https://github.com/Jaoovit">João Vitor de Oliveira
+            <FaGithub className="transition text-4xl hover:-translate-y-2 duration-300" /></a>
       </footer>
     </div>
   )
